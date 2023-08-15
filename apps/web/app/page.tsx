abb2a42b707479065c07bd7619b9628aebbfd3cd
Louis Nyirongo <louisnyirongo73@gmail.com>
2023-08-15 07:16:59 +0200

    formik form component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,17 @@
+"use client";
 import { Button, Header } from "ui";
+import { MainButton } from "shared-ui/src";
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
       <Button />
+      <MainButton
+        title="Primary"
+        variant="secondary"
+        onClick={() => console.log("test")}
+      />
     </>
   );
 }
