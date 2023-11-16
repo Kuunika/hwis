2a6b138f387a31c702d9053b272d58576b3c0c58
Louis Nyirongo <louisnyirongo73@gmail.com>
2023-11-16 10:36:07 +0200

    allergies container

--- apps/aetc/app/patient/components/index.ts
+++ apps/aetc/app/patient/components/index.ts
@@ -1,1 +1,2 @@
 export * from "./personalDetailsCard";
+export * from "./allergiesCard";
--- apps/aetc/app/patient/page.tsx
+++ apps/aetc/app/patient/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { MainGrid, NavigationBar, WrapperBox } from "shared-ui/src";
-import { PersonalDetailsCard } from "./components";
+import { AllergiesCard, PersonalDetailsCard } from "./components";
 
 export default function PatientProfile() {
   return (
@@ -9,6 +9,8 @@ export default function PatientProfile() {
       <MainGrid container spacing={1} mt={"2ch"} ml={"9ch"}>
         <MainGrid item lg={2}>
           <PersonalDetailsCard />
+          <br />
+          <AllergiesCard />
         </MainGrid>
         <MainGrid item lg={6}></MainGrid>
         <MainGrid item lg={2}></MainGrid>
