801550bc30433e131d055f5ce160f386db1d7e0e
Wisdom Changala <wisdomchangala@gmail.com>
2023-11-24 09:53:00 +0200

    mnh demographics form

--- apps/mnh/src/app/page.tsx
+++ apps/mnh/src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import {VitalsForm} from "@/app/anc/registration/vitals/components/VitalsForm";
 import {MiddlePageLayout} from "aetc/components/layouts";
+import {DemographicsForm} from "@/app/anc/registration/demographics/components/DemographicsForm";
 
 export default function Home() {
   const handleSubmit = () => {
@@ -10,8 +11,12 @@ export default function Home() {
   const initialValues = {};
 
   return (
-      <MiddlePageLayout title="Vitals">
+      <MiddlePageLayout title="Pages">
 
+        <DemographicsForm
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+        />
         <VitalsForm
             initialValues={initialValues}
             onSubmit={handleSubmit}
