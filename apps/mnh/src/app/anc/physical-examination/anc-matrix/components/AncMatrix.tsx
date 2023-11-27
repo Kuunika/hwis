777fe7b4c383a98470558bc0ba54e05145fc59b0
Crispas Makanani <crispasmakanani@gmail.com>
2023-11-27 12:57:03 +0200

    ancMatrix update

--- apps/mnh/src/app/anc/physical-examination/anc-matrix/components/AncMatrix.tsx
+++ apps/mnh/src/app/anc/physical-examination/anc-matrix/components/AncMatrix.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { FormikInit } from 'shared-ui/src';
+import React,{useState} from 'react';
+import { FieldsContainer, FormikInit, RadioGroupInput, TextInputField } from 'shared-ui/src';
+import * as yup from "yup";
 
 type Props ={
     onSubmit: () => void;
@@ -22,8 +23,8 @@ const form = {
     name: "coughtInfo",
     label: "Does the woman have a cough?",
   },
-  durationInfo: {
-    name: "durationInfo",
+  coughDuration: {
+    name: "coughDuration",
     label: "Duration of cough",
   },
   weightInfo: {
@@ -44,10 +45,132 @@ const form = {
   },
 };
 
+const schema ={
+    [form.pallorInfo.name]:yup.string().required().label(form.pallorInfo.label),
+    [form.oedemaPresent.name]:yup.string().required().label(form.oedemaPresent.label),
+    [form.severityInfo.name]:yup.string().required().label(form.severityInfo.label),
+    [form.coughInfo.name]:yup.string().required().label(form.coughInfo.label),
+    [form.coughDuration.name]:yup.string().required().label(form.coughDuration.label),
+    [form.weightInfo.name]:yup.string().required().label(form.weightInfo.label),
+    [form.feverInfo.name]:yup.string().required().label(form.feverInfo.label),
+    [form.nightSweatsInfo.name]:yup.string().required().label(form.nightSweatsInfo.label),
+}
+
 const AncMatrix = ({onSubmit,initialValues}:Props) => {
+    const [isOedema, setOedema] = useState(false);
+    const [isCough, setCough]= useState(false)
+
+    const [showOedema, setShowOedema] = useState(false);
+    const [showCough, setShowCough] = useState(false)
+
+    const handleOedemaChange = (fieldName:string, value: string)=>{
+        if(fieldName === form.oedemaPresent.name && value === "yes"){
+            setOedema(true);
+            setShowOedema(true);
+        }else{
+            setOedema(false);
+            setShowOedema(false);
+        }
+    }
+
+    const handleCoughChange = (fieldName:string, value: string)=>{
+        if(fieldName === form.coughInfo.name && value === "yes"){
+            setCough(true);
+            setShowCough(true)
+        }else{
+            setCough(false);
+            setShowCough(false)
+        }
+    }
   return (
-    <FormikInit></FormikInit>
+    <FormikInit
+      validationSchema={schema}
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      submitButtonText="next"
+    >
+      <FieldsContainer>
+        <RadioGroupInput
+          name={form.pallorInfo.name}
+          label={form.pallorInfo.label}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+      </FieldsContainer>
+      <FieldsContainer>
+        <RadioGroupInput
+          name={form.oedemaPresent.name}
+          label={form.oedemaPresent.label}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+          getValue={(value) =>
+            handleOedemaChange(form.oedemaPresent.name, value)
+          }
+        />
+        {showOedema && (
+          <RadioGroupInput
+            name={form.severityInfo.name}
+            label={form.severityInfo.label}
+            options={[
+              { label: "+", value: "+" },
+              { label: "++", value: "++" },
+              { label: "+++", value: "+++" },
+            ]}
+          />
+        )}
+      </FieldsContainer>
+      <FieldsContainer>
+        <RadioGroupInput
+          name={form.coughInfo.name}
+          label={form.coughInfo.label}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+          getValue={(value) => handleCoughChange(form.coughInfo.name, value)}
+        />
+        {showCough && (
+          <>
+            <TextInputField
+              name={form.coughDuration.name}
+              label={form.coughDuration.label}
+              id={form.coughDuration.name}
+            />
+            <RadioGroupInput
+              name={form.weightInfo.name}
+              label={form.weightInfo.label}
+              options={[
+                { label: "Yes", value: "yes" },
+                { label: "No", value: "no" },
+              ]}
+            />
+            <RadioGroupInput
+              name={form.feverInfo.name}
+              label={form.feverInfo.label}
+              options={[
+                { label: "Yes", value: "yes" },
+                { label: "No", value: "no" },
+              ]}
+            />
+            <RadioGroupInput
+              name={form.nightSweatsInfo.name}
+              label={form.nightSweatsInfo.label}
+              options={[
+                { label: "Yes", value: "yes" },
+                { label: "No", value: "no" },
+              ]}
+            />
+          </>
+        )}
+      </FieldsContainer>
+    </FormikInit>
   );
 }
 
 export default AncMatrix;
+
+
