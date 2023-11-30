d5967cffcf9e2ae818180c898dbe2713380b504c
Louis Nyirongo <louisnyirongo73@gmail.com>
2023-11-30 10:38:10 +0200

    circulation assessment

--- apps/aetc/app/primary-assessment/components/Circulation.tsx
+++ apps/aetc/app/primary-assessment/components/Circulation.tsx
@@ -1,9 +1,14 @@
 "use client";
-import React from "react";
-import { FormikInit, RadioGroupInput, TextInputField } from "shared-ui/src";
+import React, { useState } from "react";
+import {
+  FieldsContainer,
+  FormValuesListener,
+  FormikInit,
+  MainTypography,
+  RadioGroupInput,
+  TextInputField,
+} from "shared-ui/src";
 import * as yup from "yup";
-import { Box } from "@mui/material";
-import { formStyles } from "../../../../triage/src/components/forms/forms.styles";
 
 type Prop = {
   onSubmit: (values: any) => void;
@@ -72,6 +77,26 @@ const form = {
     name: "femurAndTibiaNormalInfo",
     label: "is the femur and tibia normal?",
   },
+  bleedingActionDone: {
+    name: "bleedingActionDone",
+    label: "Action done",
+  },
+  sizeOfIntravenousCatheter: {
+    name: "sizeOfIntravenousCatheter",
+    label: "Size of intravenous catheter",
+  },
+  anyOtherAbnormalities: {
+    name: "anyOtherAbnormalities",
+    label: "Is There Any Other Abnormalites",
+  },
+  anyOtherAbnormalitiesOnAbdomen: {
+    name: "anyOtherAbnormalitiesOnAbdomen",
+    label: "Is There Any Other Abnormalities on the Abdomen",
+  },
+  additionalNotes: {
+    name: "additionalNotes",
+    label: "Additional Notes",
+  },
 };
 
 const schema = yup.object({
@@ -81,7 +106,10 @@ const schema = yup.object({
     .label(form.bleedingInfo.label),
   [form.pulseInfo.name]: yup.string().required().label(form.pulseInfo.label),
 
-  [form.pulseRate.name]: yup.string().required().label(form.pulseRate.label),
+  [form.pulseRate.name]: yup.string().label(form.pulseRate.label),
+  [form.bleedingActionDone.name]: yup
+    .string()
+    .label(form.bleedingActionDone.label),
   [form.bloodPresure.name]: yup
     .string()
     .required()
@@ -121,7 +149,6 @@ const schema = yup.object({
     .string()
     .required()
     .label(form.mucousMembranesInfo.label),
-
   [form.catheterInfo.name]: yup
     .string()
     .required()
@@ -130,6 +157,16 @@ const schema = yup.object({
     .string()
     .required()
     .label(form.femurAndTibiaNormalInfo.label),
+  [form.sizeOfIntravenousCatheter.name]: yup
+    .string()
+    .label(form.sizeOfIntravenousCatheter.label),
+  [form.anyOtherAbnormalities.name]: yup
+    .string()
+    .label(form.anyOtherAbnormalities.label),
+  [form.anyOtherAbnormalitiesOnAbdomen.name]: yup
+    .string()
+    .label(form.anyOtherAbnormalitiesOnAbdomen.label),
+  [form.additionalNotes.name]: yup.string().label(form.additionalNotes.label),
 });
 
 const initialValues = {
@@ -154,23 +191,36 @@ const initialValues = {
   femurAndTibiaNormalInfo: "",
 };
 
+const sizeOfCatheter = [
+  { label: "14G", value: "14G" },
+  { label: "16G", value: "16G" },
+  { label: "18G", value: "18G" },
+  { label: "20G", value: "20G" },
+  { label: "22G", value: "22G" },
+];
+const sizeOfCapillary = [
+  { label: "Less than 3 seconds", value: "Less than 3 seconds" },
+  { label: "3 Seconds", value: "3 Seconds" },
+];
+const sizeOfMucous = [
+  { label: "Normal", value: "Normal" },
+  { label: "Pale", value: "Pale" },
+  { label: "Cyanosis", value: "Cyanosis" },
+];
+
+const pulseRates = [
+  { label: "Weak", value: "weak" },
+  { label: "Strong, Regular", value: "strong, Regular" },
+  { label: "Irregular", value: "irregular" },
+];
+
+const capillaryRefillTimes = [
+  { label: "Less than 3 seconds", value: "Less than 3 seconds" },
+  { label: "3 seconds", value: "3 seconds" },
+  { label: "More than 3 seconds", value: "More than 3 seconds" },
+];
 export const Circulation = ({ onSubmit }: Prop) => {
-  const sizeOfCatheter = [
-    { label: "14G", value: "14G" },
-    { label: "16G", value: "16G" },
-    { label: "18G", value: "18G" },
-    { label: "20G", value: "20G" },
-    { label: "22G", value: "22G" },
-  ];
-  const sizeOfCapillary = [
-    { label: "Less than 3 seconds", value: "Less than 3 seconds" },
-    { label: "3 Seconds", value: "3 Seconds" },
-  ];
-  const sizeOfMucous = [
-    { label: "Normal", value: "Normal" },
-    { label: "Pale", value: "Pale" },
-    { label: "Cyanosis", value: "Cyanosis" },
-  ];
+  const [formValues, setFormValues] = useState<any>({});
   return (
     <FormikInit
       validationSchema={schema}
@@ -178,7 +228,8 @@ export const Circulation = ({ onSubmit }: Prop) => {
       onSubmit={onSubmit}
       submitButtonText="next"
     >
-      <Box sx={formStyles.responsiveness}>
+      <FormValuesListener getValues={setFormValues} />
+      <FieldsContainer>
         <RadioGroupInput
           name={form.bleedingInfo.name}
           label={form.bleedingInfo.label}
@@ -187,7 +238,6 @@ export const Circulation = ({ onSubmit }: Prop) => {
             { label: "No", value: "no" },
           ]}
         />
-
         <RadioGroupInput
           name={form.pulseInfo.name}
           label={form.pulseInfo.label}
@@ -196,23 +246,47 @@ export const Circulation = ({ onSubmit }: Prop) => {
             { label: "No", value: "no" },
           ]}
         />
-        <TextInputField
-          name={form.pulseRate.name}
-          label={form.pulseRate.label}
-          id={form.pulseRate.name}
-        />
-        <TextInputField
-          name={form.bloodPresure.name}
-          label={form.bloodPresure.label}
-          id={form.bloodPresure.name}
-        />
+      </FieldsContainer>
+      {formValues[form.bleedingInfo.name] == "yes" && (
+        <>
+          <br />
+          <MainTypography>apply pressure</MainTypography>
+          <br />
+          <TextInputField
+            name={form.bleedingActionDone.name}
+            id={form.bleedingActionDone.name}
+            label={form.bleedingActionDone.label}
+          />
+        </>
+      )}
+
+      {formValues[form.pulseInfo.name] == "yes" && (
+        <>
+          <br />
+          <FieldsContainer>
+            <RadioGroupInput
+              name={form.pulseRate.name}
+              label={form.pulseRate.label}
+              options={pulseRates}
+            />
+          </FieldsContainer>
+        </>
+      )}
+
+      {formValues[form.pulseInfo.name] == "no" && (
+        <>
+          <br />
+          <MainTypography>start cardiopulmonary resuscitation</MainTypography>
+          <br />
+        </>
+      )}
+
+      <br />
+      <FieldsContainer>
         <RadioGroupInput
-          name={form.interavenousInfo.name}
-          label={form.interavenousInfo.label}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
+          name={form.capillaryInfo.name}
+          label={form.capillaryInfo.label}
+          options={capillaryRefillTimes}
         />
         <RadioGroupInput
           name={form.intravenousAccess.name}
@@ -222,6 +296,25 @@ export const Circulation = ({ onSubmit }: Prop) => {
             { label: "No", value: "no" },
           ]}
         />
+      </FieldsContainer>
+
+      {formValues[form.intravenousAccess.name] == "yes" && (
+        <>
+          <br />
+          <FieldsContainer>
+            <RadioGroupInput
+              name={form.catheterInfo.name}
+              label={form.catheterInfo.label}
+              options={sizeOfCatheter}
+            />
+            <MainTypography>Diagram</MainTypography>
+          </FieldsContainer>
+          <br />
+        </>
+      )}
+
+      <br />
+      <FieldsContainer sx={{ alignItems: "flex-start" }}>
         <RadioGroupInput
           name={form.traumatizedInfo.name}
           label={form.traumatizedInfo.label}
@@ -231,6 +324,50 @@ export const Circulation = ({ onSubmit }: Prop) => {
           ]}
         />
         <RadioGroupInput
+          name={form.mucousMembranesInfo.name}
+          label={form.mucousMembranesInfo.label}
+          options={sizeOfMucous}
+        />
+      </FieldsContainer>
+      <br />
+
+      {formValues[form.traumatizedInfo.name] == "yes" && (
+        <>
+          <FieldsContainer>
+            <RadioGroupInput
+              name={form.pelvisInfo.name}
+              label={form.pelvisInfo.label}
+              options={[
+                { label: "Yes", value: "yes" },
+                { label: "No", value: "no" },
+              ]}
+            />
+            <RadioGroupInput
+              name={form.femurAndTibiaNormalInfo.name}
+              label={form.femurAndTibiaNormalInfo.label}
+              options={[
+                { label: "Yes", value: "yes" },
+                { label: "No", value: "no" },
+              ]}
+            />
+          </FieldsContainer>
+
+          {formValues[form.femurAndTibiaNormalInfo.name] == "no" && (
+            <>
+              <br />
+              <MainTypography>
+                {" "}
+                posterior and anterior, with the following options for the
+                sections of the legs{" "}
+              </MainTypography>
+              <br />
+            </>
+          )}
+        </>
+      )}
+
+      <FieldsContainer>
+        <RadioGroupInput
           name={form.abdnomenDistention.name}
           label={form.abdnomenDistention.label}
           options={[
@@ -239,6 +376,30 @@ export const Circulation = ({ onSubmit }: Prop) => {
           ]}
         />
         <RadioGroupInput
+          name={form.anyOtherAbnormalitiesOnAbdomen.name}
+          label={form.anyOtherAbnormalitiesOnAbdomen.label}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+      </FieldsContainer>
+
+      {formValues[form.anyOtherAbnormalitiesOnAbdomen.name] == "yes" && (
+        <>
+          <br />
+          <MainTypography>
+            picture of abdomen to select areas of abnormalites with the options
+            below:1. Right Hyphochondriac 4. Right Lumbar 7. Right iliac 2.
+            Epigrastric 5. Umbilical 3. Left Hypochondriac6.Left Lumbar 9. Left
+            Iliac 8. Suprapubic/Hypogastric
+          </MainTypography>
+          <br />
+        </>
+      )}
+
+      <FieldsContainer>
+        <RadioGroupInput
           name={form.abnormalitiesInfo.name}
           label={form.abnormalitiesInfo.label}
           options={[
@@ -246,46 +407,30 @@ export const Circulation = ({ onSubmit }: Prop) => {
             { label: "No", value: "no" },
           ]}
         />
-        <RadioGroupInput
-          name={form.capillaryInfo.name}
-          label={form.capillaryInfo.label}
-          options={sizeOfCapillary}
-        />
-        <TextInputField
-          name={form.diastolicInfo.name}
-          label={form.diastolicInfo.label}
-          id={form.diastolicInfo.name}
-        />
-        <RadioGroupInput
-          name={form.catheterInfo.name}
-          label={form.catheterInfo.label}
-          options={sizeOfCatheter}
-        />
-        <RadioGroupInput
-          name={form.pelvisInfo.name}
-          label={form.pelvisInfo.label}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-        <RadioGroupInput
-          name={form.mucousMembranesInfo.name}
-          label={form.mucousMembranesInfo.label}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-        <RadioGroupInput
-          name={form.femurAndTibiaNormalInfo.name}
-          label={form.femurAndTibiaNormalInfo.label}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-      </Box>
+      </FieldsContainer>
+
+      {formValues[form.abnormalitiesInfo.name] == "yes" && (
+        <>
+          <br />
+          <MainTypography>
+            (Picture Comes Up and indicate the following) Tenderness (Multiple
+            Selection) Wound (Multiple Selection) Laceration Stab/Puncture
+            Bruise Burns Wound
+          </MainTypography>
+          <br />
+        </>
+      )}
+
+      <TextInputField
+        name={form.bloodPresure.name}
+        label={form.bloodPresure.label}
+        id={form.bloodPresure.name}
+      />
+      <TextInputField
+        name={form.additionalNotes.name}
+        label={form.additionalNotes.label}
+        id={form.additionalNotes.name}
+      />
     </FormikInit>
   );
 };
