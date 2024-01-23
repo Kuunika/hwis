271faf9cb85b0b0dcc89c000a640d075ca9ec164
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-01-23 22:45:32 +0200

    patients waiting for triage

--- apps/aetc/app/triage/page.tsx
+++ apps/aetc/app/triage/page.tsx
@@ -2,11 +2,44 @@
 import { MiddlePageLayout } from "@/components/layouts";
 
 import { ClientWaitingForTriage } from "./components";
+import { MainGrid } from "shared-ui/src";
+import { InitialRegistrationList } from "../initial-registration/components";
+import {
+  RegistrationMainHeader,
+  RegistrationDescriptionText,
+  RegistrationCard,
+} from "../registration/components/common";
+import { Navigation } from "../registration/scanner/page";
 
 export default function Triage() {
   return (
-    <MiddlePageLayout title="Patient Waiting For Triage" middleGridSize={8}>
-      <ClientWaitingForTriage />
-    </MiddlePageLayout>
+    <>
+      <Navigation title="Patients waiting for Triage" link="/" />
+      <MainGrid container>
+        <MainGrid lg={2} item></MainGrid>
+        <MainGrid
+          lg={8}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+          item
+        >
+          <br />
+          <br />
+          <RegistrationMainHeader>
+            Patients waiting for Triage
+          </RegistrationMainHeader>
+          <RegistrationDescriptionText>
+            This is a list of all patients that went through registration
+            successfully and waiting for triage.
+          </RegistrationDescriptionText>
+          <RegistrationCard sx={{ p: 0 }}>
+            <ClientWaitingForTriage />
+          </RegistrationCard>
+        </MainGrid>
+        <MainGrid lg={2} item></MainGrid>
+      </MainGrid>
+    </>
   );
 }
