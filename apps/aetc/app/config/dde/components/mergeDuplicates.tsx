2905e3af7306cd59d273fd1de2b4b5155666b00f
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-03-01 10:54:37 +0200

    highlight selected

--- apps/aetc/app/config/dde/components/mergeDuplicates.tsx
+++ apps/aetc/app/config/dde/components/mergeDuplicates.tsx
@@ -1,51 +1,61 @@
 import { MainButton, MainTypography, WrapperBox } from "shared-ui/src"
 import { FaRegCheckSquare, FaRegSquare, FaSearch } from "react-icons/fa";
+import { useState } from "react";
 
+const results = [
+    {
+        id: "1",
+        firstName: "John",
+        lastName: "Doe",
+        district: "Ntcheu",
+        homeTA: "Chikulamayembe",
+        village: "vingula",
+        gender: "M",
+        dateofbirth: "08-01-1994",
+        selected: true
+    },
+    {
+        id: "2",
+        firstName: "Jane",
+        lastName: "Doe",
+        district: "Blantyre",
+        homeTA: "Chikulamayembe",
+        village: "vingula",
+        gender: "M",
+        dateofbirth: "08-01-1994",
+        selected: false
+    },
+    {
+        id: "3",
+        firstName: "Anold",
+        lastName: "Banda",
+        district: "Blantyre",
+        homeTA: "Chikulamayembe",
+        village: "vingula",
+        gender: "M",
+        dateofbirth: "08-01-1994",
+        selected: false
+    }
+]
 export const MergeDuplicates = () => {
+    const [patientResult, setPatientResult] = useState<Array<any>>(results);
 
-    const results = [
-        {
-            id: "1",
-            firstName: "John",
-            lastName: "Doe",
-            district: "Ntcheu",
-            homeTA: "Chikulamayembe",
-            village: "vingula",
-            gender: "M",
-            dateofbirth: "08-01-1994"
-        },
-        {
-            id: "2",
-            firstName: "Jane",
-            lastName: "Doe",
-            district: "Blantyre",
-            homeTA: "Chikulamayembe",
-            village: "vingula",
-            gender: "M",
-            dateofbirth: "08-01-1994"
-        },
-        {
-            id: "3",
-            firstName: "Anold",
-            lastName: "Banda",
-            district: "Blantyre",
-            homeTA: "Chikulamayembe",
-            village: "vingula",
-            gender: "M",
-            dateofbirth: "08-01-1994"
-        }
-    ]
-
+    const handleSelect = (id: string) => {
+        const patients = [...patientResult];
+        const index = patients.findIndex(p => p.id == id);
+        patients[index].selected = !patients[index].selected;
+        setPatientResult(patients)
+    }
 
 
     return <WrapperBox>
-        {results.map((r) => <ResultRow person={r} key={r.id} />)}
+        {patientResult.map((r) => <ResultRow onClick={handleSelect} person={r} key={r.id} />)}
     </WrapperBox>
 }
 
-const ResultRow = ({ person }: { person: any }) => {
-    return <WrapperBox sx={{ display: "flex", p: "1ch", borderRadius: "2px", my: "1ch", cursor: "pointer", justifyContent: "space-between", "&:hover": { backgroundColor: "#DDE0E5" } }}>
-        <WrapperBox><MainTypography variant="h6"><FaRegSquare /></MainTypography></WrapperBox>
+const ResultRow = ({ person, onClick }: { person: any, onClick: (id: string) => void }) => {
+    return (<WrapperBox onClick={() => onClick(person.id)} sx={{ display: "flex", p: "1ch", borderRadius: "2px", my: "1ch", cursor: "pointer", justifyContent: "space-between", backgroundColor: person.selected ? "#DDE0E5" : "", "&:hover": { backgroundColor: "#DDE0E5" } }}>
+        <WrapperBox><MainTypography variant="h6">{person.selected ? <FaRegCheckSquare /> : <FaRegSquare />}</MainTypography></WrapperBox>
         <WrapperBox sx={{ ml: "1ch", width: "70%" }}>
             <LabelValue label="Name" value={`${person.firstName} ${person.lastName}(${person.gender}) ${person.dateofbirth} `} />
             <LabelValue label="Home District" value={person.district} />
@@ -55,7 +65,7 @@ const ResultRow = ({ person }: { person: any }) => {
         <WrapperBox>
             <MainButton variant="secondary" title={"reassign Id"} onClick={() => { }} />
         </WrapperBox>
-    </WrapperBox>
+    </WrapperBox>)
 }
 
 const LabelValue = ({ label, value }: { label: string, value: string }) => {
