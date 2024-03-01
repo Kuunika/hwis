8e03cf09d2dbac0b7f4bf98d1ca29db75879a0d2
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-03-01 09:51:52 +0200

    dashboard

--- apps/aetc/app/config/dde/page.tsx
+++ apps/aetc/app/config/dde/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { SearchForm } from "@/app/registration/search/components/searchForm";
-import { WrapperBox } from "shared-ui/src";
+import { MainTypography, WrapperBox } from "shared-ui/src";
 import { MergeDuplicates } from "./components/mergeDuplicates";
 
 export default function Page() {
 
     return <>
         <WrapperBox sx={{ width: "70%" }}>
+            <MainTypography variant="h5">Search Patient</MainTypography>
+            <br />
             <SearchForm onSubmit={() => { }} />
             <MergeDuplicates />
         </WrapperBox>
