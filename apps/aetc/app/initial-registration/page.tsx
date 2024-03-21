0e558ec6362f8462963e29c8ef4dee31b7cafbc6
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-03-21 15:37:49 +0200

    initial registration

--- apps/aetc/app/initial-registration/page.tsx
+++ apps/aetc/app/initial-registration/page.tsx
@@ -181,7 +181,7 @@ function InitialRegistration() {
           </RegistrationDescriptionText>
           {completed == 4 && (
             <OperationSuccess
-                title={`Patient Created and assigned to number  ${visitNumberResponse?.next_visit_number}`}
+              title={`Patient Created With Visit Number ${visitNumberResponse?.next_visit_number}`}
               primaryActionText="Register More Patient"
               secondaryActionText="Go Home"
               onPrimaryAction={() => {
