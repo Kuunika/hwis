f52f43509bfbd56a09a983cc31423bf9da4e37f1
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-04-18 08:44:25 +0200

    clean ups

--- apps/aetc/app/page.tsx
+++ apps/aetc/app/page.tsx
@@ -1,13 +1,10 @@
 'use client'
-
-import { BarcodeDialog } from "./initial-registration/components/barcodeScanner";
 import { LoginForm } from "./login/components/loginForm";
 
 export default function LoginPage() {
-  // return <BarcodeScanner />
+
 
   return <>
-    {/* <BarcodeDialog open={true}  onClose={() => { }} /> */}
     <LoginForm />;
   </>
 }
