9190d298d31d3b97bff81ee89a4d34030c328e86
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-04-22 11:14:24 +0200

    paginate false

--- apps/aetc/services/users.ts
+++ apps/aetc/services/users.ts
@@ -6,6 +6,5 @@ const endPoint = "/users";
 export const createUser = (patientData: any) =>
     create(patientData, endPoint);
 
-export const getUsers = () => getAll<User[]>(endPoint);
+export const getUsers = () => getAll<User[]>(`${endPoint}?paginate=false`);
 export const searchUser = (username: string) => getAll<User[]>(endPoint + "?username=" + username);
-
