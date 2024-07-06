158bf2dbd3f36c2a1d117c00bf5f445a77e0dc45
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-07-06 23:49:26 +0200

    big numbers of hours to day

--- aetc/components/CalculateWaitingTime.tsx
+++ aetc/components/CalculateWaitingTime.tsx
@@ -1,32 +1,34 @@
 import { getCATTime } from "@/helpers/dateTime";
 
 export function CalculateWaitingTime({
+  arrival_time,
+}: {
+  arrival_time: string; 
+}) {
+  const currentTime: any = getCATTime();
+  const arrivalTime: number = Date.parse(arrival_time);
+  const differenceInMilliseconds: number = currentTime - arrivalTime;
 
-    arrival_time,
-  }: {
+  let waitingTime: string;
 
-    arrival_time: any;
-  }) {
-  
-    const currentTime: any = getCATTime();
-  
-    const differenceInMilliseconds = currentTime - Date.parse(arrival_time);
-  
-    let waitingTime;
-  
-    const seconds = Math.floor(differenceInMilliseconds / 1000);
-    if (seconds < 60) {
-      waitingTime = `${seconds} seconds`;
+  const seconds: number = Math.floor(differenceInMilliseconds / 1000);
+  if (seconds < 60) {
+    waitingTime = `${seconds} seconds`;
+  } else {
+    const minutes: number = Math.floor(seconds / 60);
+    if (minutes < 60) {
+      waitingTime = `${minutes} minutes`;
     } else {
-      const minutes = Math.floor(seconds / 60);
-      if (minutes < 60) {
-        waitingTime = `${minutes} minutes`;
-      } else {
-        const hours = Math.floor(minutes / 60);
+      const hours: number = Math.floor(minutes / 60);
+      if (hours < 24) {
         waitingTime = `${hours} hours`;
+      } else {
+        const days: number = Math.floor(hours / 24);
+        const remainingHours: number = hours % 24;
+        waitingTime = remainingHours > 0 ? `${days} days ${remainingHours} hours` : `${days} days`;
       }
     }
-  
-    return waitingTime;
   }
-  
+
+  return waitingTime;
+}
