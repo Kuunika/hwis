4fa1c350ee77f28c822e3a3189e237e597748a67
Mwayanjana Bolokonya <mwayab@gmail.com>
2024-07-23 13:15:38 +0200

    added linechart component

---
+++ aetc/app/patient/components/lineChart.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+const formatDate = (value) => {
+  if (value == null) return 'NaN';
+  const date = new Date(value);
+  return `${date.getUTCDate()}/${date.getUTCMonth() + 1}/${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`;
+};
+
+const LineChartComponent = ({ chartData, xAxisData, series, height = 300 }) => {
+  return (
+    <LineChart
+      xAxis={[{ scaleType: "utc", data: xAxisData, valueFormatter: (value) => (value == null ? 'NaN' : formatDate(value)) }]}
+      series={series.map((s, index) => ({
+        data: chartData[s.key],
+        label: s.label,
+        color: s.color,
+        valueFormatter: (value) => (value == null ? 'NaN' : value.toString()),
+      }))}
+      height={height}
+      margin={{ top: 50, bottom: 30 }}
+    />
+  );
+};
+
+export default LineChartComponent;
+
