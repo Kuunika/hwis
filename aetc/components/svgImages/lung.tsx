19ca09c3eac4a064e5c98e81ff3575fb619f24d1
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-08-09 09:42:11 +0200

    remove imports from lung.tsx

--- aetc/components/svgImages/lung.tsx
+++ aetc/components/svgImages/lung.tsx
@@ -1,27 +1,27 @@
 import { ReactSVG } from "react-svg";
 import styles from "./styles/lung.module.css";
 import { useEffect } from "react";
-import {ReactComponent as ImportedSVG} from "../../assets/lung.svg"
+
 
 export const LungImage = () => {
-  useEffect(() => {
-    const rightMiddleLobe = document.getElementById("right_middle_lobe-6");
-    if (rightMiddleLobe) {
-      rightMiddleLobe.style.fill = "yellow";
-      rightMiddleLobe.style.cursor = "pointer";
-    }
-    console.log(rightMiddleLobe)
-  }, []);
+    useEffect(() => {
+        const rightMiddleLobe = document.getElementById("right_middle_lobe-6");
+        if (rightMiddleLobe) {
+            rightMiddleLobe.style.fill = "yellow";
+            rightMiddleLobe.style.cursor = "pointer";
+        }
+        console.log(rightMiddleLobe)
+    }, []);
 
-  return (
-    <div>
-      <ReactSVG
-        src={"/lung.svg"}
-        beforeInjection={(svg) => {
-          svg.setAttribute("width", "500");
-          svg.setAttribute("height", "500");
-        }}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <ReactSVG
+                src={"/lung.svg"}
+                beforeInjection={(svg) => {
+                    svg.setAttribute("width", "500");
+                    svg.setAttribute("height", "500");
+                }}
+            />
+        </div>
+    );
 };
