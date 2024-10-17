a7003f1adfd90a7be540739c61451ae1d1952e57
Isazaki <kondwanimsiska@gmail.com>
2024-10-17 11:49:58 +0200

    consultation plan done

---
+++ aetc/app/patient/[id]/consultation/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { FormContainer } from "@/components";
+import { PatientInfoTab } from "@/components";
+import { roles } from "@/constants";
+import AuthGuard from "@/helpers/authguard";
+import { StartConsultationFlow } from "./components/startConsultationFlow";
+
+function Consultation() {
+  return (
+    <>
+      <PatientInfoTab />
+      <FormContainer>
+        <StartConsultationFlow />
+      </FormContainer>
+    </>
+  );
+}
+
+export default Consultation;
+// export default AuthGuard(PrimaryAssessment, [roles.CLINICIAN, roles.NURSE, roles.ADMIN]);
