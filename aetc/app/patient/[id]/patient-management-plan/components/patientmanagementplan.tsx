abe4c5b6e24e5ab03d77bb1f0f7496ab7963792c
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-10-21 13:14:04 +0200

    medication

--- aetc/app/patient/[id]/patient-management-plan/components/patientmanagementplan.tsx
+++ aetc/app/patient/[id]/patient-management-plan/components/patientmanagementplan.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { useState } from "react";
-import { MedicationsForm } from "./medication"; // Import the MedicationsForm
-
-
+// Import the MedicationsForm
 
 export const PatientManagementPlan = () => {
-
-  return  <MedicationsForm onSkip={()=>{}} onSubmit={()=>{}} />
-};  
+  return <></>;
+};
