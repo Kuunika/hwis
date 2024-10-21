ca99ff1fef34dd868b62e23bd7969d3f0a45af07
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-10-21 14:01:43 +0200

    remove build errors

--- aetc/app/medical-history/components/admissionsForm.tsx
+++ aetc/app/medical-history/components/admissionsForm.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { FormDatePicker, MainButton, SearchComboBox, TextInputField, WrapperBox, FormFieldContainer, FormValuesListener, FormikInit } from "@/components";
+import {
+  FormDatePicker,
+  MainButton,
+  SearchComboBox,
+  TextInputField,
+  WrapperBox,
+  FormFieldContainer,
+  FormValuesListener,
+  FormikInit,
+} from "@/components";
 import * as yup from "yup";
 import DynamicFormList from "@/components/form/dynamicFormList";
 import { TableCell } from "@mui/material";
@@ -34,11 +43,11 @@ const admissionsFormConfig = {
     name: `interventions_${index}`,
     label: `Interventions ${index + 1}`,
   }),
-  discharge_instructions:  (index: number) => ({
+  discharge_instructions: (index: number) => ({
     name: `discharge_instructions_${index}`,
     label: `Discharge Instructions ${index + 1}`,
   }),
-  follow_up_plans:  (index: number) => ({
+  follow_up_plans: (index: number) => ({
     name: `follow_up_plans_${index}`,
     label: `Follow-up Plans ${index + 1}`,
   }),
@@ -47,15 +56,14 @@ const admissionsFormConfig = {
 export const AdmissionsForm = ({ onSubmit, onSkip }: Prop) => {
   const [formValues, setFormValues] = useState<any>({});
   const [admissions, setAdmissions] = React.useState([
-    { 
-      date: "", 
-      hospitals: "", 
-      wards: "", 
-      diagnoses: "", 
-      interventions: "", 
-      discharge_instructions: "", 
-      follow_up_plan: "", 
-
+    {
+      date: "",
+      hospitals: "",
+      wards: "",
+      diagnoses: "",
+      interventions: "",
+      discharge_instructions: "",
+      follow_up_plan: "",
     },
   ]);
 
@@ -88,96 +96,123 @@ export const AdmissionsForm = ({ onSubmit, onSkip }: Prop) => {
   };
 
   return (
-    <FormikInit validationSchema={schema} initialValues={initialValues} onSubmit={onSubmit} submitButton={false}>
+    <FormikInit
+      validationSchema={schema}
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      submitButton={false}
+    >
       <FormValuesListener getValues={setFormValues} />
       <DynamicFormList
-          items={admissions}
-          setItems={setAdmissions}
-          newItem={{  date: "", 
-            hospitals: "", 
-            wards: "", 
-            diagnoses: "", 
-            interventions: "", 
-            discharge_instructions: "", 
-            follow_up_plan: "", }}
-          headings={["Admission Date", "Hospital", "Ward(s)", "Diagnoses", "Interventions", "Discharge instructions","follow up plan"]}
-          renderFields={(admission, index) => (
-            <>
-              {/* Admission Date */}
-              <TableCell sx={{ width: "25%", textAlign: "center" }}>
-                <FormDatePicker
-                  name={admissionsFormConfig.admission_dates(index).name}
-                  label=""
-                  sx={{ background: "white", width: "100%" }}
-                />
-              </TableCell>
+        items={admissions}
+        setItems={setAdmissions}
+        newItem={{
+          date: "",
+          hospitals: "",
+          wards: "",
+          diagnoses: "",
+          interventions: "",
+          discharge_instructions: "",
+          follow_up_plan: "",
+        }}
+        headings={[
+          "Admission Date",
+          "Hospital",
+          "Ward(s)",
+          "Diagnoses",
+          "Interventions",
+          "Discharge instructions",
+          "follow up plan",
+        ]}
+        renderFields={(admission, index) => (
+          <>
+            {/* Admission Date */}
+            <TableCell sx={{ width: "25%", textAlign: "center" }}>
+              <FormDatePicker
+                name={admissionsFormConfig.admission_dates(index).name}
+                label=""
+                sx={{ background: "white", width: "100%" }}
+              />
+            </TableCell>
 
-              {/* Hospital */}
-              <TableCell sx={{ width: "25%", textAlign: "center" }}>
-                <SearchComboBox
-                  name={admissionsFormConfig.hospitals(index).name}
-                  label=""
-                  options={hospitalOptions}
-                  multiple={false}
-                  sx={{ width: "100%" }}
-                />
-              </TableCell>
+            {/* Hospital */}
+            <TableCell sx={{ width: "25%", textAlign: "center" }}>
+              <SearchComboBox
+                name={admissionsFormConfig.hospitals(index).name}
+                label=""
+                //@ts-ignore
+                options={hospitalOptions}
+                multiple={false}
+                sx={{ width: "100%" }}
+              />
+            </TableCell>
 
-              {/* Ward */}
-              <TableCell sx={{ width: "25%", textAlign: "center" }}>
-                <SearchComboBox
-                  name={admissionsFormConfig.wards(index).name}
-                  label=""
-                  options={wardOptions}
-                  multiple={false}
-                  sx={{ width: "100%" }}
-                />
-              </TableCell>
+            {/* Ward */}
+            <TableCell sx={{ width: "25%", textAlign: "center" }}>
+              <SearchComboBox
+                name={admissionsFormConfig.wards(index).name}
+                label=""
+                //@ts-ignore
+                options={wardOptions}
+                multiple={false}
+                sx={{ width: "100%" }}
+              />
+            </TableCell>
 
-              {/* Diagnosis */}
-              <TableCell sx={{ width: "25%", textAlign: "center" }}>
-                <SearchComboBox
-                  name={admissionsFormConfig.diagnoses(index).name}
-                  label=""
-                  options={diagnosisOptions}
-                  multiple={false}
-                  sx={{ width: "100%" }}
-                />
-              </TableCell>
-              <TableCell sx={{ width: "25%", textAlign: "center" }}>
+            {/* Diagnosis */}
+            <TableCell sx={{ width: "25%", textAlign: "center" }}>
+              <SearchComboBox
+                name={admissionsFormConfig.diagnoses(index).name}
+                label=""
+                //@ts-ignore
+                options={diagnosisOptions}
+                multiple={false}
+                sx={{ width: "100%" }}
+              />
+            </TableCell>
+            <TableCell sx={{ width: "25%", textAlign: "center" }}>
               <TextInputField
-                    id={admissionsFormConfig.interventions(index).name}
-                    name={admissionsFormConfig.interventions(index).name}
-                    label={admissionsFormConfig.interventions(index).label}
-                    multiline
-                    rows={4}
-                />
-              </TableCell>
-              <TableCell sx={{ width: "25%", textAlign: "center" }}>
+                id={admissionsFormConfig.interventions(index).name}
+                name={admissionsFormConfig.interventions(index).name}
+                label={admissionsFormConfig.interventions(index).label}
+                multiline
+                rows={4}
+              />
+            </TableCell>
+            <TableCell sx={{ width: "25%", textAlign: "center" }}>
               <TextInputField
-                    id={admissionsFormConfig.discharge_instructions(index).name}
-                    name={admissionsFormConfig.discharge_instructions(index).name}
-                    label={admissionsFormConfig.discharge_instructions(index).label}
-                    multiline
-                    rows={4}
-                />
-              </TableCell>
-              <TableCell sx={{ width: "25%", textAlign: "center" }}>
+                id={admissionsFormConfig.discharge_instructions(index).name}
+                name={admissionsFormConfig.discharge_instructions(index).name}
+                label={admissionsFormConfig.discharge_instructions(index).label}
+                multiline
+                rows={4}
+              />
+            </TableCell>
+            <TableCell sx={{ width: "25%", textAlign: "center" }}>
               <TextInputField
-            id={admissionsFormConfig.follow_up_plans(index).name}
-            name={admissionsFormConfig.follow_up_plans(index).name}
-            label={admissionsFormConfig.follow_up_plans(index).label}
-            multiline
-            rows={4}
-          />
-              </TableCell>
-            </>
-          )}
-        />
-   
-        <MainButton sx={{ m: 0.5 }} title={"Submit"} type="submit" onClick={handleSubmit} />
-        <MainButton variant={"secondary"} title="Skip" type="button" onClick={onSkip} />
+                id={admissionsFormConfig.follow_up_plans(index).name}
+                name={admissionsFormConfig.follow_up_plans(index).name}
+                label={admissionsFormConfig.follow_up_plans(index).label}
+                multiline
+                rows={4}
+              />
+            </TableCell>
+          </>
+        )}
+      />
 
+      <MainButton
+        sx={{ m: 0.5 }}
+        title={"Submit"}
+        type="submit"
+        onClick={handleSubmit}
+      />
+      <MainButton
+        variant={"secondary"}
+        title="Skip"
+        type="button"
+        onClick={onSkip}
+      />
     </FormikInit>
   );
 };
--- aetc/app/medical-history/components/familyHistoryForm.tsx
+++ aetc/app/medical-history/components/familyHistoryForm.tsx
@@ -1,186 +1,206 @@
+// @ts-nocheck
 import {
-    FormikInit,
-    FormValuesListener,
-    MainButton,
-    WrapperBox,
-    FormFieldContainer,
-    TextInputField,
-    
-  } from "@/components";
-  import { useState } from "react";
-  import * as yup from "yup";
-  import LabelledCheckbox from "@/components/form/labelledCheckBox";
-  
-  type Prop = {
-    onSubmit: (values: any) => void;
-    onSkip: () => void;
+  FormikInit,
+  FormValuesListener,
+  MainButton,
+  WrapperBox,
+  FormFieldContainer,
+  TextInputField,
+} from "@/components";
+import { useState } from "react";
+import * as yup from "yup";
+import LabelledCheckbox from "@/components/form/labelledCheckBox";
+
+type Prop = {
+  onSubmit: (values: any) => void;
+  onSkip: () => void;
+};
+
+const familyHistoryFormConfig = {
+  asthma: { name: "asthma", label: "Asthma" },
+  hypertension: { name: "hypertension", label: "Hypertension" },
+  diabetes_mellitus: { name: "diabetes_mellitus", label: "Diabetes mellitus" },
+  epilepsy: { name: "epilepsy", label: "Epilepsy" },
+  cancer: { name: "cancer", label: "Cancer" },
+  tuberculosis: { name: "tuberculosis", label: "Tuberculosis" },
+  other: { name: "other", label: "Other (Specify)" },
+};
+
+export const FamilyHistoryForm = ({ onSubmit, onSkip }: Prop) => {
+  const [formValues, setFormValues] = useState<any>({});
+  const [showRelationshipFields, setShowRelationshipFields] = useState({
+    asthma: false,
+    hypertension: false,
+    diabetes_mellitus: false,
+    epilepsy: false,
+    cancer: false,
+    tuberculosis: false,
+    other: false,
+  });
+
+  const schema = yup.object().shape({
+    asthma: yup.boolean(),
+    asthmaRelationship: yup.string().when("asthma", {
+      is: true,
+      then: yup.string().required("Please specify relationship for asthma"),
+    }),
+    hypertension: yup.boolean(),
+    hypertensionRelationship: yup.string().when("hypertension", {
+      is: true,
+      then: yup
+        .string()
+        .required("Please specify relationship for hypertension"),
+    }),
+    diabetes_mellitus: yup.boolean(),
+    diabetesMellitusRelationship: yup.string().when("diabetes_mellitus", {
+      is: true,
+      then: yup
+        .string()
+        .required("Please specify relationship for diabetes mellitus"),
+    }),
+    epilepsy: yup.boolean(),
+    epilepsyRelationship: yup.string().when("epilepsy", {
+      is: true,
+      then: yup.string().required("Please specify relationship for epilepsy"),
+    }),
+    cancer: yup.boolean(),
+    cancerType: yup.string().when("cancer", {
+      is: true,
+      then: yup.string().required("Please specify the type of cancer"),
+    }),
+    cancerRelationship: yup.string().when("cancer", {
+      is: true,
+      then: yup.string().required("Please specify relationship for cancer"),
+    }),
+    tuberculosis: yup.boolean(),
+    tuberculosisRelationship: yup.string().when("tuberculosis", {
+      is: true,
+      then: yup
+        .string()
+        .required("Please specify relationship for tuberculosis"),
+    }),
+    other: yup.boolean(),
+    otherSpecify: yup.string().when("other", {
+      is: true,
+      then: yup.string().required("Please specify the other condition"),
+    }),
+    otherRelationship: yup.string().when("other", {
+      is: true,
+      then: yup
+        .string()
+        .required("Please specify relationship for the other condition"),
+    }),
+  });
+
+  const initialValues = {
+    asthma: false,
+    hypertension: false,
+    diabetes_mellitus: false,
+    epilepsy: false,
+    cancer: false,
+    tuberculosis: false,
+    other: false,
   };
-  
-  const familyHistoryFormConfig = {
-    asthma: { name: "asthma", label: "Asthma" },
-    hypertension: { name: "hypertension", label: "Hypertension" },
-    diabetes_mellitus: { name: "diabetes_mellitus", label: "Diabetes mellitus" },
-    epilepsy: { name: "epilepsy", label: "Epilepsy" },
-    cancer: { name: "cancer", label: "Cancer" },
-    tuberculosis: { name: "tuberculosis", label: "Tuberculosis" },
-    other: { name: "other", label: "Other (Specify)" },
+
+  const handleCheckboxChange = (e: any, field: string) => {
+    const isChecked = e.target.checked;
+    setShowRelationshipFields((prev) => ({
+      ...prev,
+      [field]: isChecked,
+    }));
+    setFormValues((prev) => ({
+      ...prev,
+      [field]: isChecked,
+    }));
   };
-  
-  export const FamilyHistoryForm = ({ onSubmit, onSkip }: Prop) => {
-    const [formValues, setFormValues] = useState<any>({});
-    const [showRelationshipFields, setShowRelationshipFields] = useState({
-      asthma: false,
-      hypertension: false,
-      diabetes_mellitus: false,
-      epilepsy: false,
-      cancer: false,
-      tuberculosis: false,
-      other: false,
-    });
-  
-    const schema = yup.object().shape({
-      asthma: yup.boolean(),
-      asthmaRelationship: yup.string().when("asthma", {
-        is: true,
-        then: yup.string().required("Please specify relationship for asthma"),
-      }),
-      hypertension: yup.boolean(),
-      hypertensionRelationship: yup.string().when("hypertension", {
-        is: true,
-        then: yup.string().required("Please specify relationship for hypertension"),
-      }),
-      diabetes_mellitus: yup.boolean(),
-      diabetesMellitusRelationship: yup.string().when("diabetes_mellitus", {
-        is: true,
-        then: yup.string().required("Please specify relationship for diabetes mellitus"),
-      }),
-      epilepsy: yup.boolean(),
-      epilepsyRelationship: yup.string().when("epilepsy", {
-        is: true,
-        then: yup.string().required("Please specify relationship for epilepsy"),
-      }),
-      cancer: yup.boolean(),
-      cancerType: yup.string().when("cancer", {
-        is: true,
-        then: yup.string().required("Please specify the type of cancer"),
-      }),
-      cancerRelationship: yup.string().when("cancer", {
-        is: true,
-        then: yup.string().required("Please specify relationship for cancer"),
-      }),
-      tuberculosis: yup.boolean(),
-      tuberculosisRelationship: yup.string().when("tuberculosis", {
-        is: true,
-        then: yup.string().required("Please specify relationship for tuberculosis"),
-      }),
-      other: yup.boolean(),
-      otherSpecify: yup.string().when("other", {
-        is: true,
-        then: yup.string().required("Please specify the other condition"),
-      }),
-      otherRelationship: yup.string().when("other", {
-        is: true,
-        then: yup.string().required("Please specify relationship for the other condition"),
-      }),
-    });
-  
-    const initialValues = {
-      asthma: false,
-      hypertension: false,
-      diabetes_mellitus: false,
-      epilepsy: false,
-      cancer: false,
-      tuberculosis: false,
-      other: false,
-    };
-  
-    const handleCheckboxChange = (e: any, field: string) => {
-      const isChecked = e.target.checked;
-      setShowRelationshipFields((prev) => ({
-        ...prev,
-        [field]: isChecked,
-      }));
-      setFormValues((prev) => ({
-        ...prev,
-        [field]: isChecked,
-      }));
-    };
-  
-    const handleSubmit = () => {
-      console.log(formValues);
-      onSubmit(formValues);
-    };
-  
-    return (
-      <FormikInit
-        validationSchema={schema}
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        submitButton={false}
-      >
-        <FormValuesListener getValues={setFormValues} />
-        <FormFieldContainer direction="row">
-          <WrapperBox sx={{ bgcolor: "white", padding: "2ch", mb: "2ch", width:'100%' }}>
-            {Object.keys(familyHistoryFormConfig).map((key) => (
-              <div key={key}>
-                <LabelledCheckbox
-                  label={familyHistoryFormConfig[key].label}
-                  checked={formValues[key] || false}
-                  onChange={(e) => handleCheckboxChange(e, key)}
-                />
-                {showRelationshipFields[key] && (
-                  <>
-                    {key === "cancer" ? (
-                      <>
-                        <TextInputField
-                        id="cancerType"
-                          label="Type of Cancer"
-                          name="cancerType"
-                          placeholder="Specify type of cancer"
-                          sx={{mr:'2ch'}}
-                        />
-                        <TextInputField
-                        id="cancerRelationship"
-                          label="Relationship to family member"
-                          name="cancerRelationship"
-                          placeholder="e.g., Mother"
-                        />
-                      </>
-                    ) : key === "other" ? (
-                      <>
-                        <TextInputField
-                        id="otherSpecify"
-                          label="Specify Other Condition"
-                          name="otherSpecify"
-                          placeholder="Specify the condition"
-                          sx={{mr:'2ch'}}
-                        />
-                        <TextInputField
-                            id="otherRelationship"
-                          label="Relationship to family member"
-                          name="otherRelationship"
-                          placeholder="e.g., Mother"
-                        />
-                      </>
-                    ) : (
+
+  const handleSubmit = () => {
+    console.log(formValues);
+    onSubmit(formValues);
+  };
+
+  return (
+    <FormikInit
+      validationSchema={schema}
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      submitButton={false}
+    >
+      <FormValuesListener getValues={setFormValues} />
+      <FormFieldContainer direction="row">
+        <WrapperBox
+          sx={{ bgcolor: "white", padding: "2ch", mb: "2ch", width: "100%" }}
+        >
+          {Object.keys(familyHistoryFormConfig).map((key) => (
+            <div key={key}>
+              <LabelledCheckbox
+                label={familyHistoryFormConfig[key].label}
+                checked={formValues[key] || false}
+                onChange={(e) => handleCheckboxChange(e, key)}
+              />
+              {showRelationshipFields[key] && (
+                <>
+                  {key === "cancer" ? (
+                    <>
                       <TextInputField
-                        id={`${key}Relationship`}
+                        id="cancerType"
+                        label="Type of Cancer"
+                        name="cancerType"
+                        placeholder="Specify type of cancer"
+                        sx={{ mr: "2ch" }}
+                      />
+                      <TextInputField
+                        id="cancerRelationship"
                         label="Relationship to family member"
-                        name={`${key}Relationship`}
+                        name="cancerRelationship"
                         placeholder="e.g., Mother"
                       />
-                    )}
-                  </>
-                )}
-              </div>
-            ))}
-          </WrapperBox>
-        </FormFieldContainer>
-  
-        <WrapperBox>
-          <MainButton sx={{ m: 0.5 }} title={"Submit"} type="submit" onClick={handleSubmit} />
-          <MainButton variant={"secondary"} title="Skip" type="button" onClick={onSkip} />
+                    </>
+                  ) : key === "other" ? (
+                    <>
+                      <TextInputField
+                        id="otherSpecify"
+                        label="Specify Other Condition"
+                        name="otherSpecify"
+                        placeholder="Specify the condition"
+                        sx={{ mr: "2ch" }}
+                      />
+                      <TextInputField
+                        id="otherRelationship"
+                        label="Relationship to family member"
+                        name="otherRelationship"
+                        placeholder="e.g., Mother"
+                      />
+                    </>
+                  ) : (
+                    <TextInputField
+                      id={`${key}Relationship`}
+                      label="Relationship to family member"
+                      name={`${key}Relationship`}
+                      placeholder="e.g., Mother"
+                    />
+                  )}
+                </>
+              )}
+            </div>
+          ))}
         </WrapperBox>
-      </FormikInit>
-    );
-  };
+      </FormFieldContainer>
+
+      <WrapperBox>
+        <MainButton
+          sx={{ m: 0.5 }}
+          title={"Submit"}
+          type="submit"
+          onClick={handleSubmit}
+        />
+        <MainButton
+          variant={"secondary"}
+          title="Skip"
+          type="button"
+          onClick={onSkip}
+        />
+      </WrapperBox>
+    </FormikInit>
+  );
+};
