494faaa0068e03092fdccc9e50d741898afc8afc
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-10-22 14:30:15 +0200

    secondary assessment view values

--- aetc/components/form/hooks/useFormikField.tsx
+++ aetc/components/form/hooks/useFormikField.tsx
@@ -12,6 +12,8 @@ export const useFormikField = (name: string) => {
     handleBlur,
   } = useFormikContext();
 
+  console.log({ errors });
+
   //@ts-ignore
   const value = values[name];
 
--- aetc/components/svgImages/abdomen.tsx
+++ aetc/components/svgImages/abdomen.tsx
@@ -3,15 +3,44 @@ import { useImage } from "@/hooks/useImage";
 import { SVGPopover } from "./svgPopover";
 import { Box, Button } from "@mui/material";
 export function AbdomenImage() {
-    const { handleClose, handleFormSubmit, containerRef, section, anchorEl, setAnchorEl, highlightSection, selectedSection, setSelectedSection, highlightAllSelectedSections, setIds } = useImage()
+  const {
+    handleClose,
+    handleFormSubmit,
+    containerRef,
+    section,
+    anchorEl,
+    setAnchorEl,
+    highlightSection,
+    selectedSection,
+    setSelectedSection,
+    highlightAllSelectedSections,
+    setIds,
+  } = useImage();
 
-    return <>
-        <Abdomen ref={containerRef} />
-        <SVGPopover section={section} selectedSection={selectedSection} anchorEl={anchorEl} handleClose={handleClose}>
-            <Box sx={{ display: "flex", gap: "0.2ch" }}>
-                <Button type="submit" onClick={handleFormSubmit} sx={{ borderRadius: "1px" }} variant="contained" fullWidth>Select</Button>
-                <Button sx={{ borderRadius: "1px" }} fullWidth onClick={handleClose}>Cancel</Button>
-            </Box>
-        </SVGPopover>
+  return (
+    <>
+      <Abdomen ref={containerRef} />
+      <SVGPopover
+        section={section}
+        selectedSection={selectedSection}
+        anchorEl={anchorEl}
+        handleClose={handleClose}
+      >
+        <Box sx={{ display: "flex", gap: "0.2ch" }}>
+          <Button
+            type="submit"
+            onClick={handleFormSubmit}
+            sx={{ borderRadius: "1px" }}
+            variant="contained"
+            fullWidth
+          >
+            Select
+          </Button>
+          <Button sx={{ borderRadius: "1px" }} fullWidth onClick={handleClose}>
+            Cancel
+          </Button>
+        </Box>
+      </SVGPopover>
     </>
+  );
 }
