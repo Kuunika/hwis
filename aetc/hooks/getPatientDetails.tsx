6a9d81e1b130462bc6f5eaf2737e9868f1001dbe
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-10-30 09:06:34 +0200

    get patient details hook

---
+++ aetc/hooks/getPatientDetails.tsx
@@ -0,0 +1,14 @@
+import { useParameters } from "./navigation";
+import { getPatientVisitTypes } from "./patientReg";
+
+export const getActivePatientDetails = () => {
+  const { params } = useParameters();
+  const {
+    data: patientVisits,
+    isLoading,
+    isSuccess,
+  } = getPatientVisitTypes(params?.id as string);
+  const activeVisit = patientVisits?.find((d) => !Boolean(d.date_stopped));
+
+  return { activeVisit: activeVisit?.uuid, patientId: params?.id };
+};
