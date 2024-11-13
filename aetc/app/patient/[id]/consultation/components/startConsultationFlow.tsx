c02ecff352a9829f32fb7562e7f12d6a31d69f80
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-11-13 08:34:11 +0200

    bedside test

--- aetc/app/patient/[id]/consultation/components/startConsultationFlow.tsx
+++ aetc/app/patient/[id]/consultation/components/startConsultationFlow.tsx
@@ -3,21 +3,18 @@ import React from "react";
 import { useState } from "react";
 import { NewStepperContainer } from "@/components";
 
-import { encounters } from "@/constants";
 import { useNavigation } from "@/hooks";
 import { addEncounter } from "@/hooks/encounter";
-import { LabOrderTable } from "@/app/patient/components/panels/labOrderTable";
-import { LabRequestForm } from "./labRequestForm";
-import { LabRequest } from "@/interfaces";
+
 import DiagnosisForm from "./diagnosisForm";
 import { Button } from "@mui/material";
 import { MedicationsForm } from "./medication";
 import { BedsideTestForm } from "./bedsideTestForm";
+import { TestAccordion } from "./testAccordion";
 
 export const StartConsultationFlow = () => {
   const [activeStep, setActiveStep] = useState<number>(0);
-  const { mutate } = addEncounter();
-  const { navigateTo, navigateBack } = useNavigation();
+  const { navigateBack } = useNavigation();
 
   const steps = [
     { id: 1, label: "Investigations" },
@@ -35,16 +32,7 @@ export const StartConsultationFlow = () => {
         onBack={() => navigateBack()}
       >
         <>
-          <BedsideTestForm />
-          {/* <LabRequestForm
-            onClose={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            addRequest={function (value: LabRequest): void {
-              throw new Error("Function not implemented.");
-            }}
-          /> */}
-          <LabOrderTable />
+          <TestAccordion />
           <Button onClick={() => setActiveStep(1)}>Next</Button>
         </>
         <>
