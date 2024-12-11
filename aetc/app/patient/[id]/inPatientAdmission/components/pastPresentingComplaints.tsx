228c092c3af9371c62305e42f70405285be98074
Mwayanjana Bolokonya <mwayab@gmail.com>
2024-12-11 14:50:51 +0200

    getting complaints encounters

--- aetc/app/patient/[id]/inPatientAdmission/components/pastPresentingComplaints.tsx
+++ aetc/app/patient/[id]/inPatientAdmission/components/pastPresentingComplaints.tsx
@@ -1,9 +1,22 @@
 "use client";
 import { Panel } from "@/app/patient/components/panels";
 import { WrapperBox } from "@/components";
+import { useParameters } from "@/hooks";
+import { getPatientsEncounters } from "@/hooks/encounter";
+import { useEffect } from "react";
 
 function PresentingComplaintsPanel() {
-  
+    const { params } = useParameters();
+    const { data, isLoading } = getPatientsEncounters(params?.id as string);
+
+    const complaintsEncounters = data?.filter(
+        (item) => item.encounter_type.name === "PRESENTING COMPLAINTS"
+      );
+  useEffect(() => {
+    console.log(complaintsEncounters)
+    
+  },[complaintsEncounters])
+
 return (
     <>
     
