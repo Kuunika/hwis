f3b93f1f2d2924ab4a29a3a5560635c50f50c594
Louis Nyirongo <louisnyirongo73@gmail.com>
2025-01-09 15:06:25 +0200

    cpr form vitals segment

--- aetc/app/patient/[id]/primary-assessment/components/cprDialogForm.tsx
+++ aetc/app/patient/[id]/primary-assessment/components/cprDialogForm.tsx
@@ -14,6 +14,7 @@ import { getInitialValues } from "@/helpers";
 import useFetchMedications from "@/hooks/useFetchMedications";
 import { Box, Typography } from "@mui/material";
 import { FieldArray } from "formik";
+import { useState } from "react";
 import { GiMedicines } from "react-icons/gi";
 import * as Yup from "yup";
 
@@ -30,6 +31,66 @@ const form = {
     name: concepts.OTHER,
     label: "Specify",
   },
+  reversibleCauses: {
+    name: concepts.REVERSIBLE_CAUSES,
+    label: "Reversible Causes",
+  },
+  reasonsCprStopped: {
+    name: concepts.REASON_CPR_STOPPED,
+    label: "Reason CPR Stopped",
+  },
+  otherReason: {
+    name: concepts.OTHER,
+    label: "Other Reason",
+  },
+  dispositionAfterCpr: {
+    name: concepts.DISPOSITION_AFTER_CPR,
+    label: "Disposition After CPR",
+  },
+  spo: {
+    name: concepts.SPO2,
+    label: "SPO2",
+  },
+  oxygen: {
+    name: concepts.OXYGEN_GIVEN,
+    label: "Oxygen",
+  },
+  respiratoryRate: {
+    name: concepts.RESPIRATORY_RATE,
+    label: "Respiratory Rate",
+  },
+  systolic: {
+    name: concepts.BLOOD_PRESSURE_SYSTOLIC,
+    label: "Systolic",
+  },
+  diastolic: {
+    name: concepts.BLOOD_PRESSURE_DIASTOLIC,
+    label: "Diastolic",
+  },
+  gcs: {
+    name: concepts.GCS,
+    label: "Glasgow Coma Scale",
+  },
+  pulseRate: {
+    name: concepts.PULSE_RATE,
+    label: "Pulse Rate",
+  },
+  temperature: {
+    name: concepts.TEMPERATURE,
+    label: "Temperature",
+  },
+  motorResponse: {
+    name: concepts.MOTOR_RESPONSE,
+    label: "Motor Response",
+  },
+  eyeOpeningResponse: {
+    name: concepts.EYE_OPENING_RESPONSE,
+    label: "Eye Opening Response",
+  },
+  verbalResponse: {
+    name: concepts.VERBAL_RESPONSE,
+    label: "Verbal Response",
+  },
 };
 
 const initialValues = getInitialValues(form);
@@ -38,6 +99,77 @@ const validationSchema = Yup.object().shape({
   [form.cardiacArrest.name]: Yup.string()
     .required()
     .label(form.cardiacArrest.label),
+  [form.site.name]: Yup.string().required().label(form.site.label),
+  [form.specify.name]: Yup.string().when(form.site.name, {
+    is: (value: any) => value === concepts.OTHER,
+    then: (schema) => schema.required().label(form.specify.label),
+  }),
+  records: Yup.array().of(
+    Yup.object().shape({
+      rhythm: Yup.string().required("Required"),
+      shockEnergy: Yup.string().required("Required"),
+      medication: Yup.string().required("Required"),
+      dose: Yup.string().required("Required"),
+      route: Yup.string().required("Required"),
+      Interventions: Yup.string().required("Required"),
+      occurrences: Yup.string().required("Required"),
+    })
+  ),
+  [form.reversibleCauses.name]: Yup.array()
+    .required()
+    .label(form.reversibleCauses.label),
+  [form.reasonsCprStopped.name]: Yup.string()
+    .required()
+    .label(form.reasonsCprStopped.label),
+  [form.otherReason.name]: Yup.string()
+    .when(form.reasonsCprStopped.name, {
+      is: (value: any) => value === concepts.OTHER,
+      then: (schema) => schema.required().label(form.otherReason.label),
+    })
+    .label(form.otherReason.label),
+  [form.dispositionAfterCpr.name]: Yup.string()
+    .required()
+    .label(form.dispositionAfterCpr.label),
+  [form.spo.name]: Yup.number()
+    .min(0)
+    .max(100)
+    .required()
+    .label(form.spo.label),
+  [form.oxygen.name]: Yup.string().required().label(form.oxygen.label),
+  [form.respiratoryRate.name]: Yup.number()
+    .min(0)
+    .max(90)
+    .required()
+    .label(form.respiratoryRate.label),
+  [form.systolic.name]: Yup.number()
+    .min(0)
+    .max(300)
+    .required()
+    .label(form.systolic.label),
+  [form.diastolic.name]: Yup.number()
+    .min(0)
+    .max(300)
+    .required()
+    .label(form.diastolic.label),
+  [form.gcs.name]: Yup.string().required().label(form.gcs.label),
+  [form.temperature.name]: Yup.number()
+    .min(20)
+    .max(45)
+    .required()
+    .label(form.temperature.label),
+  [form.pulseRate.name]: Yup.number()
+    .min(60)
+    .max(100)
+    .label(form.pulseRate.label),
+  [form.eyeOpeningResponse.name]: Yup.string()
+    .required()
+    .label(form.eyeOpeningResponse.label),
+  [form.motorResponse.name]: Yup.string()
+    .required()
+    .label(form.motorResponse.label),
+  [form.verbalResponse.name]: Yup.string()
+    .required()
+    .label(form.verbalResponse.label),
 });
 
 const sites = [
@@ -51,6 +183,35 @@ const rhythmOptions = [
   { label: "VF", id: concepts.VF },
   { label: "VT", id: concepts.VT },
   { label: "Asys", id: concepts.ASYS },
+];
+
+const reversibleCauses = [
+  { label: "Hypoxia", id: concepts.HYPOXIA },
+  { label: "Hypovolemia", id: concepts.HYPOVOLEMIA },
+  { label: "Hyperkalaemia", id: concepts.HYPERKALAEMIA },
+  { label: "Hypokalaemia", id: concepts.HYPOKALAEMIA },
+  { label: "Hypothermia", id: concepts.HYPOTHERMIA },
+  { label: "Tension Pneumothorax", id: concepts.TENSION_PNEUMOTHORAX },
+  { label: "Tamponade, Cadiac", id: concepts.TAMPONADE_CADIAC },
+  { label: "Toxins", id: concepts.TOXINS },
+  { label: "Thrombosis, Pulmonary", id: concepts.THROMBOSIS_PULMONARY },
+  { label: "Thrombosis, Coronary", id: concepts.THROMBOSIS_CORONARY },
+  { label: "Hydrogen Ions (Acidosis)", id: concepts.HYDROGEN_ION_ACIDOSIS },
+];
+
+const reasonsCprStopped = [
+  { label: "ROSC", id: concepts.ROSC },
+  { label: "Team decision to stop", id: concepts.TEAM_DECISION_TO_STOP },
+  { label: "DNA CPR document found", id: concepts.DNA_CPR_DOCUMENT_FOUND },
+  { label: "Other", id: concepts.OTHER },
+];
+
+const dispositionAfterCpr = [
+  { label: "Resuscitation room", id: concepts.RESUSCITATION_ROOM },
+  { label: "ICU", id: concepts.ICU },
+  { label: "HDU Specify", id: concepts.HDU_SPECIFY },
+  { label: "General Ward Specify", id: concepts.GENERAL_WARD_SPECIFY },
+  { label: "Mortuary", id: concepts.MORTUARY },
 ];
 
 const radioOptions = [
@@ -80,9 +241,40 @@ const interventions = [
   { label: "POCUS", id: concepts.POCUS },
   { label: "ABG", id: concepts.ABG },
 ];
+const eyeOpeningResponses = [
+  { label: "Spontaneous", value: "Spontaneous", weight: 4 },
+  { label: "To Speech", value: "To Speech", weight: 3 },
+  { label: "To Pain", value: "To Pain", weight: 2 },
+  { label: "No Response", value: "No Response", weight: 1 },
+];
+const motorResponses = [
+  { label: "Obeying Commands", value: "Obeying Commands", weight: 6 },
+  { label: "Localising", value: "Localising", weight: 5 },
+  { label: "Withdraw", value: "Withdraw", weight: 4 },
+  { label: "Normal Flexion", value: "Normal Flexion", weight: 3 },
+  { label: "Extension", value: "Extension", weight: 2 },
+  { label: "None", value: "None", weight: 1 },
+];
+const verbalResponses = [
+  { label: "Oriented", value: "Oriented", weight: 5 },
+  { label: "Confused", value: "Confused", weight: 4 },
+  { label: "Inappropriate Words", value: "Inappropriate Words", weight: 3 },
+  {
+    label: "Incomprehensible sounds",
+    value: "Incomprehensible sounds",
+    weight: 2,
+  },
+  { label: "None", value: "None", weight: 1 },
+];
 
 const CPRForm = () => {
   const { medicationOptions } = useFetchMedications();
+  // const [total, setTotal] = useState(0);
+  const getWeight = (value: string, lists: any) => {
+    const found = lists.find((l: any) => l.value == value);
+    return found ? found.weight : 0;
+  };
+
   return (
     <FormikInit
       onSubmit={() => {}}
@@ -201,6 +393,128 @@ const CPRForm = () => {
               ></DynamicFormList>
             )}
           </FieldArray>
+          <br />
+          <br />
+          <FormFieldContainerMultiple>
+            <SearchComboBox
+              name={form.reversibleCauses.name}
+              label={form.reversibleCauses.label}
+              options={reversibleCauses}
+              multiple
+            />
+            <SearchComboBox
+              name={form.reasonsCprStopped.name}
+              label={form.reasonsCprStopped.label}
+              options={reasonsCprStopped}
+              multiple={false}
+            />
+            <SearchComboBox
+              name={form.dispositionAfterCpr.name}
+              label={form.dispositionAfterCpr.label}
+              options={dispositionAfterCpr}
+              multiple={false}
+            />
+          </FormFieldContainerMultiple>
+          <br />
+          <Typography variant="h6">Vital signs after ROSC</Typography>
+          <br />
+
+          <FormFieldContainerMultiple>
+            <TextInputField
+              name={form.spo.name}
+              label={form.spo.label}
+              id={form.spo.name}
+              sx={{ width: "100%" }}
+              unitOfMeasure="%"
+            />
+
+            <RadioGroupInput
+              name={form.oxygen.name}
+              label={form.oxygen.label}
+              options={radioOptions}
+              row
+            />
+
+            <TextInputField
+              name={form.systolic.name}
+              label={form.systolic.label}
+              id={form.systolic.name}
+              sx={{ width: "100%" }}
+              unitOfMeasure="mmHg"
+            />
+            <TextInputField
+              name={form.diastolic.name}
+              label={form.diastolic.label}
+              id={form.diastolic.name}
+              sx={{ width: "100%" }}
+              unitOfMeasure="mmHg"
+            />
+          </FormFieldContainerMultiple>
+          <FormFieldContainerMultiple>
+            <TextInputField
+              name={form.respiratoryRate.name}
+              label={form.respiratoryRate.label}
+              id={form.respiratoryRate.name}
+              sx={{ width: "100%" }}
+              unitOfMeasure="BPM"
+            />
+            <TextInputField
+              name={form.pulseRate.name}
+              label={form.pulseRate.label}
+              id={form.pulseRate.name}
+              sx={{ width: "100%" }}
+              unitOfMeasure="BPM"
+            />
+            <TextInputField
+              name={form.temperature.name}
+              label={form.temperature.label}
+              id={form.temperature.name}
+              sx={{ width: "100%" }}
+              unitOfMeasure="°C"
+            />
+          </FormFieldContainerMultiple>
+          <br />
+
+          <FieldsContainer sx={{ alignItems: "start" }}>
+            <RadioGroupInput
+              name={form.motorResponse.name}
+              label={form.motorResponse.label}
+              options={motorResponses}
+              row={false}
+            />
+            <RadioGroupInput
+              name={form.verbalResponse.name}
+              label={form.verbalResponse.label}
+              options={verbalResponses}
+              row={false}
+            />
+            <RadioGroupInput
+              name={form.eyeOpeningResponse.name}
+              label={form.eyeOpeningResponse.label}
+              options={eyeOpeningResponses}
+              row={false}
+            />
+          </FieldsContainer>
+          <Typography fontWeight={"800"} variant="body2">
+            (M
+            {getWeight(values[form.motorResponse.name], motorResponses)} V
+            {getWeight(values[form.verbalResponse.name], verbalResponses)} E
+            {getWeight(
+              values[form.eyeOpeningResponse.name],
+              eyeOpeningResponses
+            )}
+            ){" "}
+            {getWeight(values[form.motorResponse.name], motorResponses) +
+              getWeight(values[form.verbalResponse.name], verbalResponses) +
+              getWeight(
+                values[form.eyeOpeningResponse.name],
+                eyeOpeningResponses
+              )}
+            /15
+          </Typography>
+          <br />
+          <Typography variant="h6">Resuscitation Team</Typography>
+          <br />
         </>
       )}
     </FormikInit>
