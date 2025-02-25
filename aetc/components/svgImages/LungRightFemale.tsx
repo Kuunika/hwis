e1913f57b40557f571f07a64d506eb20a7f7a9c4
Louis Nyirongo <louisnyirongo73@gmail.com>
2025-02-25 13:48:27 +0200

    lung right  female image

---
+++ aetc/components/svgImages/LungRightFemale.tsx
@@ -0,0 +1,72 @@
+import { SVGPopover } from "./svgPopover";
+import { Box } from "@mui/material";
+
+import { DataBox, RushForm } from "./forms";
+import { useImageFormTransform } from "@/hooks";
+
+import { concepts } from "@/constants";
+import { useEffect } from "react";
+
+import { useImageUpdate } from "@/hooks/useImageUpdate";
+import { FullBodyFemaleBack } from "@/assets/fullBodyFemaleBack";
+import { LungLeftFemale } from "@/assets";
+interface Props {
+  onValueChange: (values: any) => void;
+  imageEncounter?: string;
+  imageSection?: string;
+}
+
+export function LungRightFemaleImage({
+  onValueChange,
+  imageEncounter,
+  imageSection,
+}: Props) {
+  const {
+    handleClose,
+    handleFormSubmit,
+    containerRef,
+    section,
+    anchorEl,
+    selectedSection,
+    ids,
+  } = useImageUpdate();
+  const { setData, submittedValues } = useImageFormTransform();
+  useEffect(() => {
+    onValueChange(ids);
+  }, [ids]);
+
+  const handleDataSubmission = (
+    section: string,
+    formData: any,
+    formConceptsLabels: Array<{ concept: string; label: string }>
+  ) => {
+    setData({ section, formData, formConceptsLabels });
+    if (imageEncounter && imageSection) {
+      formData = {
+        ...formData,
+        [concepts.IMAGE_ENCOUNTER]: imageEncounter,
+        [concepts.IMAGE_SECTION]: imageSection,
+      };
+    }
+    handleFormSubmit(formData);
+  };
+
+  return (
+    <div>
+      <LungLeftFemale ref={containerRef} />
+      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+        {submittedValues.map((value) => (
+          <DataBox key={value.section} labelValue={value} />
+        ))}
+      </Box>
+      <SVGPopover
+        section={section}
+        selectedSection={selectedSection}
+        anchorEl={anchorEl}
+        handleClose={handleClose}
+      >
+        <></>
+      </SVGPopover>
+    </div>
+  );
+}
