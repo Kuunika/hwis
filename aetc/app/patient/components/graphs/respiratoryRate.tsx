162c62b0eb31a8fd82371ecee51dd591b9a7a36e
petroskayange <kayangepetros@gmail.com>
2025-03-05 05:23:40 +0200

    update respiratory rate

--- aetc/app/patient/components/graphs/respiratoryRate.tsx
+++ aetc/app/patient/components/graphs/respiratoryRate.tsx
@@ -1,107 +1,23 @@
-import React, { useEffect, useRef } from "react";
-import ApexCharts from "apexcharts";
-import { Grid, Box, Typography, Paper, IconButton } from "@mui/material";
-import Stack from "@mui/material/Stack";
-import SvgIcon from "@mui/material/SvgIcon";
-
+import { extractTimes } from "@/helpers/dateTime";
+import { useVitalsGraphData } from "@/hooks";
+import { LineChart } from "./lineChart";
 export function RespiratoryRate() {
-  const lineChartRef = useRef(null);
-
-  // Initialize line chart in a separate useEffect
-  useEffect(() => {
-    // Define line chart options
-    const lineChartOptions = {
-      chart: {
-        id: "vuechart-example",
-        type: "line",
-        height: 350,
-      },
-      stroke: {
-        curve: "straight",
-        width: 3,
-      },
-      dataLabels: {
-        enabled: true,
-        enabledOnSeries: undefined,
-        textAnchor: "middle",
-        distributed: false,
-        offsetX: 0,
-        offsetY: 0,
-        style: {
-          fontSize: "10px",
-          fontFamily: "Helvetica, Arial, sans-serif",
-          fontWeight: "bold",
-          colors: undefined,
-        },
-        background: {
-          enabled: true,
-          foreColor: "#fff",
-          padding: 5,
-          borderRadius: 8,
-          borderWidth: 1,
-          borderColor: "#fff",
-          opacity: 0.9,
-          dropShadow: {
-            enabled: false,
-            top: 1,
-            left: 1,
-            blur: 1,
-            color: "#000",
-            opacity: 0.45,
+  const { chartData } = useVitalsGraphData();
+  return (
+    <LineChart
+      chartConfig={{
+        series: [
+          {
+            name: "Respiratory Rate",
+            data: chartData.rrData,
           },
-        },
-        dropShadow: {
-          enabled: false,
-          top: 1,
-          left: 1,
-          blur: 1,
-          color: "#000",
-          opacity: 0.45,
-        },
-      },
-      colors: ["#70C1B3", "#d815a7"],
-      xaxis: {
-        categories: [
-          "08:00",
-          "09:00",
-          "10:00",
-          "11:00",
-          "12:00",
-          "13:00",
-          "14:00",
-          "15:00",
-          "16:00",
         ],
-      },
-      yaxis: {
-        min: 0,
-        forceNiceScale: true,
-      },
-      series: [
-        {
-          name: "HR",
-          data: [72, 75, 78, 70, 68, 80, 74, 73, 76], // Heart rate in bpm, typically 60-100
-        },
-        {
-          name: "RR",
-          data: [16, 18, 15, 17, 14, 19, 16, 15, 17], // Respiratory rate (normally 12-20 breaths per minute)
-        },
-      ],
-    };
-
-    let lineChart;
-    if (lineChartRef.current) {
-      lineChart = new ApexCharts(lineChartRef.current, lineChartOptions);
-      lineChart.render();
-    }
-
-    // Clean up line chart
-    return () => {
-      if (lineChart) {
-        lineChart.destroy();
-      }
-    };
-  }, []);
-
-  return <Box id="line-chart" ref={lineChartRef} sx={{ height: 350 }}></Box>;
+        xAxisCategories: extractTimes(chartData.xAxisData),
+        title: "",
+        height: 350,
+        yAxisMin: 0,
+        colors: ["#d815a7"],
+      }}
+    />
+  );
 }
