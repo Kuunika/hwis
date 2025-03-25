546475ceb25ae15e9ee84ff78befdb46de71535a
petroskayange <kayangepetros@gmail.com>
2025-03-25 15:09:48 +0200

    adding back to profile page faction

--- aetc/hooks/navigation.ts
+++ aetc/hooks/navigation.ts
@@ -1,14 +1,16 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
 import { useParams } from "next/navigation";
 
 export const useNavigation = () => {
   const router = useRouter();
+  const params = useParams();
 
   return {
     navigateTo: (link: string) => router.push(link),
     refresh: () => router.refresh(),
-    navigateBack: () => router.back()
+    navigateBack: () => router.back(),
+    navigateBackToProfile: () => router.push(`/patient/${params.id}/profile`),
   };
 };
 
