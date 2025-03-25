8a78b940eb479f16723b3202a4671783de9e7793
petroskayange <kayangepetros@gmail.com>
2025-03-25 18:29:25 +0200

    improve back button to be back to profile

--- aetc/components/buttons/backButton.tsx
+++ aetc/components/buttons/backButton.tsx
@@ -2,15 +2,35 @@
 import { FaArrowCircleLeft } from "react-icons/fa";
 import { MainTypography } from "@/components";
 import { useRouter } from "next/navigation";
+import { FaAngleLeft } from "react-icons/fa6";
+import { useNavigation } from "@/hooks";
 
 export function BackButton() {
   const router = useRouter();
+  const { navigateBackToProfile, navigateTo } = useNavigation();
   return (
-    <MainTypography
-      onClick={() => router.back()}
-      sx={{ cursor: "pointer", color: "grey" }}
-    >
-      <FaArrowCircleLeft />
-    </MainTypography>
+    <>
+      <MainTypography
+        onClick={() => navigateBackToProfile()}
+        style={{ display: "flex", marginBottom: "10px" }}
+      >
+        <MainTypography
+          sx={{ width: "24px", height: "24px", fontSize: "20px" }}
+        >
+          <FaAngleLeft />
+        </MainTypography>
+        <MainTypography
+          sx={{
+            fontSize: "14px",
+            fontWeight: 400,
+            lineHeight: "21px",
+            letterSpacing: "0em",
+            textAlign: "left",
+          }}
+        >
+          Back to Profile
+        </MainTypography>
+      </MainTypography>
+    </>
   );
 }
