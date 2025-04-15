2b10f528b9d75495c1793bf4ec62096c375614c6
nickson-majawa <nickson.majawa@gmail.com>
2025-04-15 09:28:28 +0200

    Updated disposition forms and  death form with demographics

--- aetc/constants/index.ts
+++ aetc/constants/index.ts
@@ -313,6 +313,7 @@ export const concepts = {
   COUNSELLING: "Counselling",
   FEEDING: "Feeding",
   OXYGENATION: "Oxygenation",
+  TAPID_SPONGING: "Tapid Sponging",
   TEPID_SPONGING: "Tepid Sponging",
   ELECTROCARDIOGRAPHY_MONITORING: "Electrocardiography Monitoring",
   TURNING_PATIENTS: "Turning Patients",
