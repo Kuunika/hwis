b0030370f6c2a2d3137ebfbc0c6759d5587e2667
Mwayanjana Bolokonya <dk0df66nqdj8fjcyy1ot941rw77cys@bots.bitbucket.org>
2025-07-15 10:52:05 +0200

    added units map and changed display format to horizontal

--- aetc/app/patient/components/panels/bedsideResults.tsx
+++ aetc/app/patient/components/panels/bedsideResults.tsx
@@ -9,6 +9,45 @@ interface BedsideResultsProps {
 export const BedsideResults = ({ data }: BedsideResultsProps) => {
   const [groupedObservations, setGroupedObservations] = useState<Obs[]>([]);
 
+  const UNIT_MAP: Record<string, string> = {
+  // Blood Gas Values
+  "pH": "",
+  "PCO2": "mmHg",
+  "PO2": "mmHg",
+  "Base Excess": "mmol/L",
+
+  // Oximetry Values
+  "ctHb": "g/dL",
+  "SO2E": "%",
+  "FO2HBE": "%",
+  "FHHBE": "%",
+  "FmetHb": "%",
+
+  // Electrolyte Values
+  "Creatine kinase": "mmol/L",
+  "CNA": "mmol/L",
+  "CA2": "mmol/L",
+  "CCL": "mmol/L",
+
+  // Metabolic Values
+  "cGlu": "mmol/L",
+  "cLac": "mmol/L",
+  "ctBil": "µmol/L",
+
+  // Temperature-Corrected Values
+  "pH (T)": "",
+  "pCO₂ (T)": "mmHg",
+  "pO₂ (T)": "mmHg",
+  "P50E": "",
+
+  // Acid Base Status
+  "BaseExcess": "mmol/L",
+  "HCO3": "mmol/L",
+  "Anion Gapc": "mmol/L",
+  "MOSMC": "mmol/kg"
+};
+
+
   useEffect(() => {
     if (data.length > 0) {
       const finalObsArray = Object.values(data);
@@ -38,20 +77,40 @@ export const BedsideResults = ({ data }: BedsideResultsProps) => {
     }
   }, [data]);
 
+  
+  const getUnit = (name: string) => {
+    const unit = UNIT_MAP[name] ?? "";
+    return `${unit ? ` ${unit}` : ""}`;
+};
+ 
   const renderObservation = (obs: Obs, level: number = 0) => {
-    const indent = level * 16;
-
-    return (
-      <div key={obs.obs_id} style={{ marginLeft: indent }}>
-        <Typography>
-          <b>{obs.names?.[0]?.name}:</b>{" "}
-          {obs.value ?? obs.value_text ?? obs.value_numeric ?? "N/A"}
-        </Typography>
-        {obs.children?.length > 0 &&
-          obs.children.map((childObs) => renderObservation(childObs, level + 1))}
-      </div>
-    );
-  };
+  const indent = level * 16;
+  return (
+    <div key={obs.obs_id} style={{ marginLeft: indent }}>
+      <Typography>
+        {obs.names?.[0]?.name === 'Description' ? "" : `${obs.names?.[0]?.name} `}
+        {obs.value_text ? (
+          <>
+            <b>{obs.value_text}</b>
+            <span>{getUnit(obs.names?.[0]?.name)}</span>
+          </>
+        ) : (
+          <>
+            <b>
+              {obs.value ?? obs.value_numeric ?? "N/A"}
+            </b>
+            <span>{getUnit(obs.names?.[0]?.name)}</span>
+          </>
+        )}
+      </Typography>
+      {obs.children?.length > 0 && (
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
+          {obs.children.map((childObs) => renderObservation(childObs, level + 1))}
+        </div>
+      )}
+    </div>
+  );
+};
 
   return (
     <div
