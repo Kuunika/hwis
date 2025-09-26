6a21306e2168457a18fd59ead90a57a298075515
Louis Nyirongo <louisnyirongo73@gmail.com>
2025-09-26 12:09:16 +0200

    ✨ feat(reports): include user_id in Metabase iframe URL for personalized reports

--- aetc/app/reports/page.tsx
+++ aetc/app/reports/page.tsx
@@ -3,13 +3,22 @@ import { BackButton } from "@/components/buttons"
 import { roles } from "@/constants"
 import AuthGuard from "@/helpers/authguard"
 import { useNavigation } from "@/hooks"
+import { jwtDecode } from "jwt-decode";
+
+type MyJwtPayload = {
+    user_id: string;
+};
 
 function Reports() {
-    const {navigateBack}=useNavigation()
+    const {navigateBack}=useNavigation();
+    const accessToken = localStorage.getItem("accessToken");
+
+   const {user_id} = jwtDecode<MyJwtPayload>(accessToken!)
+
     return <>
         <BackButton onClick={navigateBack} label="Go back" />
         <iframe
-            src={process.env.NEXT_PUBLIC_METABASE}
+            src={`${process.env.NEXT_PUBLIC_METABASE}?user_id=${user_id}`}
             width="100%"
             height="800ch"
         ></iframe>
