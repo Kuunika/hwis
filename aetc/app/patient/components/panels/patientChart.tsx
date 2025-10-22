4fb186489ec804f762286a15ab266b61a241daef
petroskayange <kayangepetros@gmail.com>
2025-10-22 17:04:44 +0200

    ✨ feat(patient): display vitals as individual components
    - replace combined vitals with individual components
    - display respiratory rate, heart rate, glucose, blood pressure, temperature, and o2 saturation

--- aetc/app/patient/components/panels/patientChart.tsx
+++ aetc/app/patient/components/panels/patientChart.tsx
@@ -1,5 +1,12 @@
 import { MainButton, MainTypography, WrapperBox } from "@/components";
-import { CombinedVitals } from "../graphs";
+import {
+  BP,
+  HeartRate,
+  RespiratoryRate,
+  Temp,
+  O_2Sat,
+  Glucose,
+} from "../graphs";
 import { FaPlus } from "react-icons/fa";
 import { Panel } from ".";
 import {
@@ -31,10 +38,58 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 export const PatientChart = () => {
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={12}>
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={4}>
         <Paper elevation={2} sx={{ p: 2 }}>
-          <CombinedVitals />
+          <Typography variant="subtitle1" sx={{ mb: 1, fontWeight: 600 }}>
+            Respiratory Rate
+          </Typography>
+          <RespiratoryRate />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} md={4}>
+        <Paper elevation={2} sx={{ p: 2 }}>
+          <Typography variant="subtitle1" sx={{ mb: 1, fontWeight: 600 }}>
+            Heart Rate
+          </Typography>
+          <HeartRate />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} md={4}>
+        <Paper elevation={2} sx={{ p: 2 }}>
+          <Typography variant="subtitle1" sx={{ mb: 1, fontWeight: 600 }}>
+            Glucose
+          </Typography>
+          <Glucose />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} md={4}>
+        <Paper elevation={2} sx={{ p: 2 }}>
+          <Typography variant="subtitle1" sx={{ mb: 1, fontWeight: 600 }}>
+            Blood Pressure
+          </Typography>
+          <BP />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} md={4}>
+        <Paper elevation={2} sx={{ p: 2 }}>
+          <Typography variant="subtitle1" sx={{ mb: 1, fontWeight: 600 }}>
+            Temperature
+          </Typography>
+          <Temp />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} md={4}>
+        <Paper elevation={2} sx={{ p: 2 }}>
+          <Typography variant="subtitle1" sx={{ mb: 1, fontWeight: 600 }}>
+            O₂ Saturation
+          </Typography>
+          <O_2Sat />
         </Paper>
       </Grid>
     </Grid>
